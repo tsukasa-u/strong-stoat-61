986f09639d387a957faa8943719ce0b4644b2157
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2023-04-02 17:30:12 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-serve((req: Request) => new Response("Hello World"));
+
+
+fetch("https://cdn.jsdelivr.net/gh/tsukasa-u/D3NS@main/font/D3DN.woff")
+  .then(response => response.blob())
+  .then(blob =>  {
+    // serve((req: Request) => blob);
+    console.log(blob);
+    var reader = new FileReader();
+    reader.onload = function() {
+        var result = new Uint8Array(reader.result);
+    };
+    reader.readAsArrayBuffer(blob);
+})
+serve((req: Request) => {
+    return new Response("Hello World");
+});
+
+
+
