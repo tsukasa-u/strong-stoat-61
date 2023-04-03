7757f96de8ea48d4c5a425ada69a36e8f01b7886
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2023-04-03 23:12:13 +0900

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,13 @@
+use crate::woff2::Woff2Decompress;
+
 mod woff2_reader;
 mod woff2;
 mod woff2_def;
 mod woff2_cmap;
+mod woff2_subtable;
 
 fn main() {
+    let mut data:[u8; 1] = Default::default();
+    Woff2Decompress(&mut data, 1);
     println!("Hello, world!");
 }
