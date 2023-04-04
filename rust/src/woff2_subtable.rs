37345177aa1023a782b5e4cf748fa2fad53e68d9
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2023-04-05 00:13:24 +0900

    Update woff2_subtable.rs

--- rust/src/woff2_subtable.rs
+++ rust/src/woff2_subtable.rs
@@ -69,9 +69,63 @@ pub struct Woff2CampSubTable0 {
     format: u16,
     offset: u16,
     length: u16,
-    src_offset: u32,
-    src_length: u32,
+    src_offset: u16,
+    src_length: u16,
     language: u16,
+}
+
+impl Woff2CampSubTable0 {
+    pub fn setFormat(&mut self, val: u16) {
+        self.format = val;
+    }
+
+    pub fn setOffset(&mut self, val: u16) {
+        self.offset = val;
+    }
+    
+    pub fn setLength(&mut self, val: u16) -> bool {
+        self.length = val;
+        return val > 0;
+    }
+    
+    pub fn setSrc_offset(&mut self, val: u16) {
+        self.src_offset = val;
+    }
+    
+    pub fn setSrc_length(&mut self, val: u16) -> bool {
+        self.src_length = val;
+        return val > 0;
+    }
+
+    pub fn setLanguage(&mut self, val: u16) {
+        self.language = val;
+    } 
+}
+
+
+pub fn subtableType0(subtable: &mut Woff2CampSubTable0, buf: &Vec<u8>, mut cnt: usize) -> bool {
+    // let mut cnt: usize = subtable.src_offset as usize + 2;
+
+    subtable.setFormat(0u16);
+    
+    let length: u16 = ReadUInt16(buf, &mut cnt);
+    if !subtable.setLength(length) { return false; }
+    subtable.setLanguage(ReadUInt16(buf, &mut cnt));
+    subtable.setSrc_offset(cnt as u16);
+
+    if length - 6 != 256 { return false; }
+    subtable.setSrc_length(256u16);
+    return true;
+}
+
+//------------------------------------------------------------
+
+#[derive(Default)]
+pub struct Woff2CampSubTable2SubHeader {
+    firstCode: u16,
+    entryCount: u16,
+    idDelta: i16,
+    idRangeOffset: u16,
 }
 
 #[derive(Default)]
@@ -79,9 +133,54 @@ pub struct Woff2CampSubTable2 {
     format: u16,
     offset: u16,
     length: u16,
-    src_offset: u32,
-    src_length: u32,
+    key_offset: u16,
+    key_length: u16,
     language: u16,
+    pub sub_headers: Vec<Woff2CampSubTable2SubHeader>,
+}
+
+impl Woff2CampSubTable2 {
+    pub fn setFormat(&mut self, val: u16) {
+        self.format = val;
+    }
+
+    pub fn setOffset(&mut self, val: u16) {
+        self.offset = val;
+    }
+    
+    pub fn setLength(&mut self, val: u16) -> bool {
+        self.length = val;
+        return val > 0;
+    }
+    
+    pub fn setKey_offset(&mut self, val: u16) {
+        self.key_offset = val;
+    }
+    
+    pub fn setKey_length(&mut self, val: u16) -> bool {
+        self.key_length = val;
+        return val > 0;
+    }
+
+    pub fn setLanguage(&mut self, val: u16) {
+        self.language = val;
+    } 
+}
+
+
+pub fn subtableType2(subtable: &mut Woff2CampSubTable2, buf: &Vec<u8>, mut cnt: usize) -> bool {
+    // let mut cnt: usize = subtable.src_offset as usize + 2;
+
+    subtable.setFormat(0u16);
+    
+    let length: u16 = ReadUInt16(buf, &mut cnt);
+    if !subtable.setLength(length) { return false; }
+    subtable.setLanguage(ReadUInt16(buf, &mut cnt));
+    // subtable.setSrc_offset(cnt as u16);
+
+    // if length - 6 != 256 { return false; }
+    // subtable.setSrc_length(256);
+    return true;
 }
 
 #[derive(Default)]
@@ -154,65 +253,8 @@ pub struct Woff2CampSubTable14 {
     language: u16,
 }
 
-impl Woff2CampSubTable0 {
-    pub fn setFormat(self: &mut Woff2CampSubTable0, val: u16) {
-        self.format = val;
-    }
 
-    pub fn setOffset(&mut self, val: u16) {
-        self.offset = val;
-    }
-    
-    pub fn setLength(&mut self, val: u16) -> bool {
-        self.offset = val;
-        return val > 0;
-    }
-    
-    pub fn setSrc_offset(&mut self, val: u16) {
-        self.offset = val;
-    }
-    
-    pub fn setSrc_length(&mut self, val: u16) -> bool {
-        self.offset = val;
-        return val > 0;
-    }
-
-    pub fn setLanguage(&mut self, val: u16) {
-        self.language = val;
-    } 
-}
-
-
-pub fn subtableType0(subtable: &mut Woff2CampSubTable0, buf: &Vec<u8>) -> bool {
-    let mut cnt: usize = subtable.src_offset as usize + 2;
-
-    subtable.setFormat(0u16);
-    
-    let length: u16 = ReadUInt16(buf, &mut cnt);
-    if !subtable.setLength(length) { return false; }
-    subtable.setLanguage(ReadUInt16(buf, &mut cnt));
-    subtable.setSrc_offset(cnt as u16);
-
-    if length - 6 != 256 { return false; }
-    subtable.setSrc_length(256);
-    return true;
-}
-
-pub fn subtableType2(subtable: &mut Woff2CampSubTable2, buf: &Vec<u8>) -> bool {
-    // let mut cnt: usize = subtable.src_offset as usize + 2;
-    
-    // subtable.setFormat(2u16);
-    
-    // let length: u16 = ReadUInt16(buf, &mut cnt);
-    // if !subtable.setLength(length) { return false; }
-    // subtable.setLanguage(ReadUInt16(buf, &mut cnt));
-    // subtable.setSrc_offset(cnt as u16);
-
-
-    return true;
-}
-
-pub fn subtableType4(subtable: &mut Woff2CampSubTable4, buf: &Vec<u8>) -> bool {
+pub fn subtableType4(subtable: &mut Woff2CampSubTable4, buf: &Vec<u8>, mut cnt: usize) -> bool {
     // let mut cnt: usize = subtable.src_offset as usize + 2;
     
     // subtable.setFormat(2u16);
@@ -220,7 +262,7 @@ pub fn subtableType4(subtable: &mut Woff2CampSubTable4, buf: &Vec<u8>) -> bool {
     return true;
 }
 
-pub fn subtableType6(subtable: &mut Woff2CampSubTable6, buf: &Vec<u8>) -> bool {
+pub fn subtableType6(subtable: &mut Woff2CampSubTable6, buf: &Vec<u8>, mut cnt: usize) -> bool {
     // let mut cnt: usize = subtable.src_offset as usize + 2;
     
     // subtable.setFormat(2u16);
@@ -228,7 +270,7 @@ pub fn subtableType6(subtable: &mut Woff2CampSubTable6, buf: &Vec<u8>) -> bool {
     return true;
 }
 
-pub fn subtableType8(subtable: &mut Woff2CampSubTable8, buf: &Vec<u8>) -> bool {
+pub fn subtableType8(subtable: &mut Woff2CampSubTable8, buf: &Vec<u8>, mut cnt: usize) -> bool {
     // let mut cnt: usize = subtable.src_offset as usize + 2;
     
     // subtable.setFormat(2u16);
@@ -236,7 +278,7 @@ pub fn subtableType8(subtable: &mut Woff2CampSubTable8, buf: &Vec<u8>) -> bool {
     return true;
 }
 
-pub fn subtableType10(subtable: &mut Woff2CampSubTable10, buf: &Vec<u8>) -> bool {
+pub fn subtableType10(subtable: &mut Woff2CampSubTable10, buf: &Vec<u8>, mut cnt: usize) -> bool {
     // let mut cnt: usize = subtable.src_offset as usize + 2;
     
     // subtable.setFormat(2u16);
@@ -244,7 +286,7 @@ pub fn subtableType10(subtable: &mut Woff2CampSubTable10, buf: &Vec<u8>) -> bool
     return true;
 }
 
-pub fn subtableType12(subtable: &mut Woff2CampSubTable12, buf: &Vec<u8>) -> bool {
+pub fn subtableType12(subtable: &mut Woff2CampSubTable12, buf: &Vec<u8>, mut cnt: usize) -> bool {
     // let mut cnt: usize = subtable.src_offset as usize + 2;
     
     // subtable.setFormat(2u16);
@@ -252,7 +294,7 @@ pub fn subtableType12(subtable: &mut Woff2CampSubTable12, buf: &Vec<u8>) -> bool
     return true;
 }
 
-pub fn subtableType13(subtable: &mut Woff2CampSubTable13, buf: &Vec<u8>) -> bool {
+pub fn subtableType13(subtable: &mut Woff2CampSubTable13, buf: &Vec<u8>, mut cnt: usize) -> bool {
     // let mut cnt: usize = subtable.src_offset as usize + 2;
     
     // subtable.setFormat(2u16);
@@ -260,7 +302,7 @@ pub fn subtableType13(subtable: &mut Woff2CampSubTable13, buf: &Vec<u8>) -> bool
     return true;
 }
 
-pub fn subtableType14(subtable: &mut Woff2CampSubTable14, buf: &Vec<u8>) -> bool {
+pub fn subtableType14(subtable: &mut Woff2CampSubTable14, buf: &Vec<u8>, mut cnt: usize) -> bool {
     // let mut cnt: usize = subtable.src_offset as usize + 2;
     
     // subtable.setFormat(2u16);
